69f2436c9119e5b8dec5ed8098c01810e5f2468e
rajitha <rajitha.puramani@gmail.com>
2024-08-28 15:42:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rajitha.... welcome to deployment react app-demo!!!!!
+          Rajitha.... welcome to deployment react-app-demo!!!!!
         </p>
         
       </header>
